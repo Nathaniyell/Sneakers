c6aa4f823f66d20fa782fc3afd3dbe8ef75af9bc
Thaniyell <essien.nathan@yahoo.com>
2022-07-21 16:25:37 +0100

    footer removed

--- slides.js
+++ slides.js
@@ -1,29 +1,21 @@
-$('#img1').click(function(){
-    $('#firstImage').attr('src', 'images/img4.jpg')
-});
-$('#img2').click(function(){
-    $('#firstImage').attr('src', 'images/img3.jpg')
-});
-$('#img3').click(function(){
-    $('#firstImage').attr('src', 'images/img2.jpg')
-})
-$('#img4').click(function(){
-    $('#firstImage').attr('src', 'images/img5.jpg')
-});
-
-$(".hamburger").click(function(){
-    $("#list").css('display', 'block')
-    $("#list").mouseleave(()=>{
-        $("#list").fadeOut(1500)
+$(document).ready(function () {
+    $('#img1').click(function () {
+        $('#firstImage').attr('src', 'images/img4.jpg')
+    });
+    $('#img2').click(function () {
+        $('#firstImage').attr('src', 'images/img3.jpg')
+    });
+    $('#img3').click(function () {
+        $('#firstImage').attr('src', 'images/img2.jpg')
     })
-});
+    $('#img4').click(function () {
+        $('#firstImage').attr('src', 'images/img5.jpg')
+    });
 
-
-    $('#footer').mouseenter(function(){
-        $('.footerDetails').css('display', 'block')
-        $('#footer').mouseleave(function(){
-            $('.footerDetails').fadeOut(1000)
+    $(".hamburger").click(function () {
+        $("#list").css('display', 'block')
+        $("#list").mouseleave(() => {
+            $("#list").fadeOut(1500)
         })
-    })
-
-
+    });
+});
